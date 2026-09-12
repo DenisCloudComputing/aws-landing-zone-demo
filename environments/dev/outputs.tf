d6204bd7f13ec6844a0e34b396626bb48ec93f15
DenisCloudComputing <1865435@bdeb.qc.ca>
2026-09-12 00:05:29 -0400

    fix: define missing outputs in vpc and ec2 modules

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,12 +1,11 @@
-
-# environments/dev/outputs.tf
-
 output "vpc_id" {
-  description = "ID du VPC dev"
-  value       = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "web_public_ip" {
-  description = "Adresse IP publique du serveur web Nginx"
-  value       = module.ec2.instance_public_ip
+  value = module.ec2.public_ip
+}
+
+output "web_url" {
+  value = "http://${module.ec2.public_ip}"
 }
