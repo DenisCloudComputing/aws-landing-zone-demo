1c21a75c9c4a6b522078082f7ab861bf0c5bc760
DenisCloudComputing <1865435@bdeb.qc.ca>
2026-09-11 22:17:22 -0400

    ci: add github actions workflows for terraform plan and apply

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+
+variable "project_name" {
+  description = "Nom du projet"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environnement"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR du VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
